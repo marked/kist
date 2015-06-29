4dad8221b9ef4db3bd215b04003d1de7fd63d23c
marked <marked@users.noreply.github.com>
2015-06-29 02:24:49 -0400

    Debug config value

--- bin/Conf.pm
+++ bin/Conf.pm
@@ -5,13 +5,15 @@ use warnings;
 use parent 'Exporter'; # imports and subclasses Exporter
 
 our $IP = "tivo";	# || $ENV{TIVO_IP};
-our $MAK= "" || $ENV{TIVO_MAK};
+our $MAK= "" 
+  || $ENV{TIVO_MAK};
 if (defined $MAK && $MAK =~ /^([0-9]+)$/) {
   $MAK =~ $1;
 } else {
   die "Invalid MAK\nDefine \$TIVO_MAK or edit Conf.pm\n";
 }
 our $SID = "kist";
+our $DEBUG = "0";
 
 our $ROOT = $INC[0] . "/..";
 our $DOC_DIR = "$ROOT/docroot";
@@ -24,6 +26,7 @@ our @EXPORT = qw(
   $IP
   $MAK
   $SID
+  $DEBUG
   $ROOT
   $DOC_DIR
   $BIN_DIR
